78db7987c1ab3e6976c5d6bf07e4dba6b3c0afa2
Virendra Arekar <virendra.arekar@gmail.com>
2024-12-16 19:27:05 +0530

    Update COde

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
